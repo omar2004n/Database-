290f83b762dde3f221aa6119ef7af8934105b7dc
omar2004n <75938226+omar2004n@users.noreply.github.com>
2023-03-26 20:43:03 +0000

    Update database.c

--- database.c
+++ database.c
@@ -14,33 +14,61 @@ typedef struct relation{
 }relation;
 
 //show_att();
-void Show_Attr(relation *r,char info){
-	int i=0;
-	if(r==NULL){
+void Show_Attr(relation *r,char info);
+void Show_Relation(relation *r);
+void link_attr(relation * r,attr *at);
+void enter_functional_dep(relation *r);
+void startscreen(relation *r);
+int check_repetitions(attr* tmp,attr *new);
+attr * create_attr(char info);
+attr *search_attr(relation *r,char info);
+void get_def(relation *r,attr *tmp);
+
+int main()
+{
+    relation *rel=(relation*)malloc(sizeof(relation));
+    rel->first=rel->last=NULL;
+    startscreen(rel);
+    return 0;
+}
+int check_attr(relation *r,char info)
+{
+    return (search_attr(r,info)==NULL)?1:0;
+}
+attr *search_attr(relation *r,char info)
+{
+    	if(r==NULL){
 		printf("\nRelation Doesn't exist\n");
-		return ;
+		return NULL;
 	}
 	if (r->first==NULL){
 		printf ("\nEmpty Relation\n");
-		return ;
+		return NULL;
 	}
-	attr * tmp = r->first;
-
-		while (tmp!= NULL||tmp->info==info){
+    attr *tmp=r->first;
+    		while (tmp!= NULL){
+            if(tmp->info==info)break;
 			tmp = tmp -> next ;
 		}
+        return tmp;
+}
+void Show_Attr(relation *r,char info){
+	    int i=0;
+        //printf("----Showing Attribute '%c'----",info);
+	    attr * tmp = NULL;
+		tmp= search_attr(r,info);
 		if(tmp==NULL){
-			printf("\nAttribute not found");
+			printf("\nAttribute not found !");
 			return ;
 		}
 		else {
-		printf("\n%c:\t ",tmp->info);
-		while(tmp->def[i+1]!=NULL)
+		printf("\n[ %c:  ",tmp->info);
+		while(tmp->def[i]!=NULL)
 		{
 			printf(" %c, ",tmp->def[i]->info);
 			i++;
 		}
-		printf("%c",tmp->def[i]->info);
+		printf("]");
 		}
 		
 	
@@ -56,6 +84,13 @@ void Show_Relation(relation *r){
       tmp = tmp->next;
    }
    printf(")\n");
+   tmp =r->first;
+   while(tmp != NULL) {
+      Show_Attr(r,tmp->info);
+      tmp = tmp->next;
+   }
+   
+   
 }
 
 
@@ -65,48 +100,119 @@ attr * create_attr(char info)
 {
 	attr *at;
     at = (attr*)malloc(sizeof(attr));
+    if(at==NULL)exit(404);
     at->info=info;
     at->next=NULL;
-    at->def=(attr**)malloc(sizeof(attr));
+    at->def=(attr**)malloc(sizeof(attr)*2);
     at->def[0]=at;
+    at->def[1]=NULL;
     return at;
 }
 
 void link_attr(relation * r,attr *at)
 {
+    if(at==NULL||r==NULL)
+    {
+        printf("\nError !");
+        return ;
+    }
     if(r->first == NULL)
     {
         r->first=r->last=at;
         at->next=NULL;
         return ;
     }
-    else {
-    	r->last->next=at;
+    
+    r->last->next=at;
     at->next=NULL;
     r->last=at;
-	}
-    
+}
 
+void enter_functional_dep(relation *r)
+{
+    attr *tmp=r->first,*d=NULL;
+    int i=3;
+    char c;
+    printf("\n--Enter Functional Dependencies (X -> Y) enter '0' if finished--\n");
+    while(tmp!=NULL)
+    {
+        c='1';
+        printf("\n%c :\t",tmp->info);
+        while(c!='0')
+        {
+            
+            c=getchar();
+            getchar();
+            d=search_attr(r,c);
+            if(d==NULL&&c!='0'){
+                printf("\n--Attribut '%c' not found !--",c);
+                continue;
+            }
+            if(check_repetitions(tmp,d))
+            continue;
+            if(c=='0')
+            break;
+
+            tmp->def=(attr**)realloc(tmp->def,i*sizeof(attr*));
+            tmp->def[i-2]=d;
+            tmp->def[i-1]=NULL;
+            i++;
+            
+        }
+        printf("\n----------------------------------------");
+        tmp=tmp->next;
+        i=3;
+    }
+}
+
+int  check_repetitions(attr *tmp,attr *new)
+{
+    int i=0;
+    
+    while(tmp->def[i]!=NULL)
+    {
+        if(tmp->def[i]==new)
+        return 1;
+        i++;
+    }
+    return 0;
+}
+int def_len(attr *tmp)
+{
+    int i=0;
+    while(tmp->def[i]!=NULL)i++;
+    return i;
+}
+void get_def(relation *r,attr *tmp)
+{
+    int i,j;
+    attr *t;
+    for(i=0;(t=tmp->def[i])!=NULL;i++)
+        {
+            for(j=0;t->def[j]==NULL;j++)
+            {
+                if(check_repetitions(tmp,t->def[j]))
+                continue;
+                
+            }
+        }
 }
 void startscreen(relation *r)
 {
     char c='c';
-    system("cls");
+    system("clear");
     printf("\nEnter all the attributs (if you're done enter 0) :\n");
     while(c!='0')
     {
         //c='0';
-    scanf("%c",&c);
-    if(c!='0')
-    link_attr(r,create_attr(c));
+    c=getchar();
+    getchar();
     
+    if(c!='0'&&c!='\n'&&c!='\t')
+    link_attr(r,create_attr(c));
+    //else printf("\nInvalid name !");
     }
-	Show_Attr(r,'a');
-}
-int main()
-{
-    relation *rel=(relation*)malloc(sizeof(relation));
-    rel->first=rel->last=NULL;
-    startscreen(rel);
-    return 0;
+    system("clear");
+    enter_functional_dep(r);
+    Show_Relation(r);
 }
